fc6b75177c78ff995645eee23a365ee36f9f0d07
Yana <yana2393@gmail.com>
2023-06-15 11:56:06 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import css from './ContactForm.module.css';
 import { addContact } from 'redux/operations.js';
 import { getContacts } from 'redux/selectors';
@@ -8,9 +8,10 @@ const ContactForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const contacts = useSelector(getContacts);
+
   const dispatch = useDispatch();
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     const fieldName = event.target.name;
     const fieldValue = event.target.value;
 
@@ -18,10 +19,11 @@ const ContactForm = () => {
       setName(fieldValue);
     } else if (fieldName === 'number') {
       setNumber(fieldValue);
-    } return
+    }
+    return;
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
     if (name.trim() === '' || number.trim() === '') {
       return;
@@ -31,26 +33,27 @@ const ContactForm = () => {
       name: name.trim(),
       number: number.trim(),
     };
-    const action = addContact(newContact)
 
+    if (contacts.contacts.find(contact => contact.name === newContact.name)) {
+      setName('');
+      setNumber('');
+      alert(`${newContact.name} is already in contacts!`);
+      return;
+    }
+
+    const action = addContact(newContact);
     dispatch(action);
 
     setName('');
     setNumber('');
-
-    if (contacts.name === newContact.name) { 
-      setName('');
-      setNumber('');
-    };
-    alert(`${newContact.name} is already in contacts!`);
-    return;
   };
 
   return (
     <div className={css.formWrapper}>
       <form className={css.form} onSubmit={handleSubmit}>
         <p className={css.txt}>Name</p>
-        <input className={css.input}
+        <input
+          className={css.input}
           type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -60,7 +63,8 @@ const ContactForm = () => {
           onChange={handleChange}
         />
         <p className={css.txt}>Number</p>
-        <input className={css.input}
+        <input
+          className={css.input}
           type="tel"
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
